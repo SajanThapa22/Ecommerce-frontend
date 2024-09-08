844a491ca633cbe8fe617d9edc2bcb32cdfa160f
SajanThapa22 <sajanthapa1888@gmail.com>
2024-09-08 19:15:49 +0545

    authentication

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { Product } from "../page";
+import ProductCard from "@/components/ProductCard";
 
-const page = () => {
-  return <div>page</div>;
+const page = async () => {
+  // const userDetail = {
+
+  // }
+  // const authResponse = await fetch('http://localhost:3000/api/auth')
+
+  useEffect(() => {
+    const fetchCart = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/cart", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCart();
+  });
+
+  // const data: Product[] = await res.json();
+
+  return (
+    <div>
+      no items
+      {/* {data.length > 0
+        ? data?.map((item) => (
+            <ProductCard
+              key={item._id}
+              _id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          ))
+        : "no items found"} */}
+    </div>
+  );
 };
 
 export default page;
