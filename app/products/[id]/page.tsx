56ad20999b38020f3b8cd19e35f26fad9b8ecfa0
SajanThapa22 <sajanthapa1888@gmail.com>
2024-09-13 15:37:23 +0545

    fix: dynamic route for product details

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,34 +1,17 @@
-"use client";
-
-import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
 import Button from "@/components/Button";
 import { Product } from "@/app/page";
 
-const ProductDetails = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [product, setProduct] = useState<Product>();
-  const [error, setError] = useState<string>("");
+const ProductDetails = async ({ params }: { params: { id: string } }) => {
+  const res = await fetch(`http://localhost:3000/api/products/${params.id}`, {
+    next: { revalidate: 10 }, // Optional: Revalidate every 10 seconds
+  });
 
-  useEffect(() => {
-    async function fetchProduct() {
-      if (id) {
-        try {
-          const res = await fetch(`http://localhost:3000/api/products/${id}`);
-          if (res.status === 200) {
-            const result = await res.json();
-            setProduct(result.data);
-          } else {
-            setError("something went wrong");
-          }
-        } catch (err) {
-          console.error("an error occurred", err);
-        }
-      }
-    }
-    fetchProduct();
-  }, []);
+  if (!res.ok) {
+    // Handle errors (e.g., product not found)
+    return <div>Error fetching product details</div>;
+  }
+
+  const product: Product = await res.json();
 
   return (
     <>
@@ -36,11 +19,13 @@ const ProductDetails = () => {
         <div>
           <img className="w-full object-cover" src={product?.image} alt="" />
         </div>
-        <div id="details" className="flex flex-col gap-4 font-sans">
+        <div id="details" className="flex flex-col gap-3 font-sans">
           <div className="text-[18px] text-black font-[600]">
             {product?.name}
           </div>
-          <div className="opacity-80 text-sm">{product?.description}</div>
+          <div className="opacity-80 text-[16px] text-black">
+            {product?.description}
+          </div>
           <div className="text-[16px]">{product?.price}</div>
 
           <Button text="Buy now" style="bg-tomato" />
