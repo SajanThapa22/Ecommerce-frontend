3e6dc819a0719facbd15d28a6bf3c6514750550c
SajanThapa22 <sajanthapa1888@gmail.com>
2024-09-27 21:06:16 +0545

    adding featured products in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaShippingFast, FaMoneyBillWave, FaHeadset } from "react-icons/fa";
 import Link from "next/link";
 import FeatureCard from "@/components/FeatureCard";
 import Button from "@/components/Button";
 import Reveal from "@/components/Reveal";
+import { useTimer } from "react-timer-hook";
+import ProductCard from "@/components/ProductCard";
 
 export interface Props {
   addToCart: () => void;
@@ -40,7 +42,30 @@ const features = [
   },
 ];
 
-function Home({ featuredProducts, addToCart }: Props) {
+function Home({ addToCart }: Props) {
+  const [featuredProducts, setFeaturedProducts] = useState<Product[]>();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(
+          "http://localhost:3000/api/products?page=4&limit=3"
+        );
+        const data = await res.json();
+        setFeaturedProducts(data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  });
+
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 86400); // 24 hours = 86400 seconds
+
+  const { seconds, minutes, hours, isRunning } = useTimer({
+    expiryTimestamp: time,
+    onExpire: () => console.log("Timer expired"),
+  });
   return (
     <div className="w-full mx-auto py-[2rem] px-[7rem] bg-orange-50">
       <Reveal>
@@ -61,6 +86,14 @@ function Home({ featuredProducts, addToCart }: Props) {
           </div>
         </section>
       </Reveal>
+
+      <div className="w-full text-center">
+        <p className="text-[32px] text-center text-black">{`${hours
+          .toString()
+          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+          .toString()
+          .padStart(2, "0")}`}</p>
+      </div>
       <Reveal>
         <section className="grid grid-cols-responsive gap-[2rem] mb-[2rem] text-black">
           {features.map((f) => {
@@ -76,15 +109,18 @@ function Home({ featuredProducts, addToCart }: Props) {
       <Reveal>
         <section className="mb-[32px] w-full text-center text-black">
           <h2 className="text-[32px] font-semibold">Featured Products</h2>
-          {/* <div className="grid grid-cols-responsive">
-          {featuredProducts.map((product) => (
-            <ProductCard
-              key={product.id}
-              product={product}
-              addToCart={addToCart}
-            />
-          ))}
-        </div> */}
+          <div className="grid grid-cols-responsive">
+            {featuredProducts?.map((product) => (
+              <ProductCard
+                key={product._id}
+                _id={product._id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                image={product.image}
+              />
+            ))}
+          </div>
         </section>
       </Reveal>
       <Reveal>
